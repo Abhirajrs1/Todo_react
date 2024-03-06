3fd9555e85790d927f246a2f7eb6c94f5c327bf1
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2024-03-06 10:52:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Heading from './components/Heading'
+function App() {
+
+  return (
+    <>
+   <Heading/>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Heading.jsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
+import { TiTick } from "react-icons/ti";
+import './Heading.css'
+function Heading() {
+    const [isCompleScreen, setIsCompleteScreen] = useState(false)
+    const [toDo, setToDo] = useState([])
+    const [newTitle, setNewTitle] = useState('')
+    const [newDescription, setNewDescription] = useState('')
+    const [completedTodos, setCompletedTodos] = useState([])
+
+    const todo = () => {
+        setIsCompleteScreen(false)
+    }
+    const completed = () => {
+        setIsCompleteScreen(true)
+    }
+    const setTitle = (e) => {
+        setNewTitle(e.target.value)
+    }
+    const setDescription = (e) => {
+        setNewDescription(e.target.value)
+    }
+
+    const handleAddTodo = () => {
+        let newTodo = {
+            title: newTitle,
+            description: newDescription
+        }
+        let updatedTodoArray = [...toDo]
+        updatedTodoArray.unshift(newTodo)
+        setToDo(updatedTodoArray)
+        localStorage.setItem('todolist', JSON.stringify(updatedTodoArray))
+        setNewTitle('')
+        setNewDescription("")
+
+    }
+
+    const handleDeleteTodo = (index) => {
+        const updatedTodoArray = toDo.filter((_, i) => i !== index)
+        setToDo(updatedTodoArray)
+        localStorage.setItem('todolist', JSON.stringify(updatedTodoArray))
+    }
+
+    const handleComplete = (index) => {
+        let now = new Date()
+        let dd = now.getDate()
+        let mm = now.getMonth() + 1
+        let yyyy = now.getFullYear()
+        let h = now.getHours()
+        let m = now.getMinutes()
+        let s = now.getSeconds()
+        let completedOn = dd + '-' + mm + '-' + yyyy + ' at ' + h + ':' + m + ':' + s
+        let filteredItem = {
+            ...toDo[index],
+            completedOn: completedOn
+        }
+        let updatedCompleteArr = [...completedTodos]
+        updatedCompleteArr.unshift(filteredItem)   
+        setCompletedTodos(updatedCompleteArr)
+        handleDeleteTodo(index)
+        localStorage.setItem('completedTodos', JSON.stringify(updatedCompleteArr))
+    }
+    const handleDeleteCompletedTodo = (index) => {
+        let reducedTodoArray = completedTodos.filter((_, i) => i !== index);
+        setCompletedTodos(reducedTodoArray);
+        localStorage.setItem('completedTodos', JSON.stringify(reducedTodoArray));
+    }
+
+   
+    useEffect(() => {
+        let savedTodo = JSON.parse(localStorage.getItem('todolist'))
+        let savedCompletedTodo = JSON.parse(localStorage.getItem('completedTodos'))
+        if (savedTodo) {
+            setToDo(savedTodo)
+        }
+        if (savedCompletedTodo) {
+            setCompletedTodos(savedCompletedTodo)
+        }
+    }, [])
+
+
+
+    return (
+        <div className="Main">
+            <h1>MY TODO</h1>
+            <div className="Wrapper">
+                <div className="todo_input">
+                    <div className="input_items">
+                        <label htmlFor="">TITLE:</label>
+                        <input type="text" value={newTitle} placeholder="Title of your Todo" onChange={setTitle} />
+                    </div>
+                    <div className="input_items">
+                        <label htmlFor="">DESCRIPTION:</label>
+                        <input type="text" value={newDescription} placeholder="Please enter the description" onChange={setDescription} />
+                    </div>
+                    <div className="input_items">
+                        <button type="button" className="primarybutton" onClick={handleAddTodo}>ADD</button>
+                    </div>
+                </div>
+
+                <div className="button_area">
+                    <button className={`secondaryButton ${isCompleScreen === false && 'active'}`} onClick={todo}>To-DO</button>
+                    <button className={`secondaryButton ${isCompleScreen === true && 'active'}`} onClick={completed}>COMPLETED</button>
+                </div>
+
+                <div className="todo_list">
+
+                    {isCompleScreen === false && toDo.map((item, index) => {
+                        return (
+                            <div className="todo_list_items" key={index}>
+                                <div>
+                                    <h3>{item.title}</h3>
+                                    <p>{item.description}</p>
+                                </div>
+                                <div>
+                                    <MdDelete className="icon" onClick={() => handleDeleteTodo(index)} title="Delete?" />
+                                    <TiTick className="check-icon" onClick={() => handleComplete(index)} title="Complete?" />
+                                </div>
+
+
+                            </div>
+                        )
+                    })}
+
+                    {isCompleScreen === true && completedTodos.map((item, index) => {
+                        return (
+                            <div className="todo_list_items" key={index}>
+                                <div>
+                                    <h3>{item.title}</h3>
+                                    <p>{item.description}</p>
+                                    <p><small>Completed on : {item.completedOn}</small></p>
+
+                                </div>
+
+                                <div>
+                                    <MdDelete className="icon" onClick={() => handleDeleteCompletedTodo(index)} title="Delete?" />
+                                </div>
+
+
+                            </div>
+                        )
+                    })}
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Heading;
